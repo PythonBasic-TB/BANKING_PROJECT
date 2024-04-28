2366f86f76b4181c784581c4470939b5e1e3c511
paatakill <paata.kilasonia@gmail.com>
2024-04-29 01:49:52 +0400

    validators Cavucere ibanis checki da davumate tanxis ase vtqvat "isfloat" checki

--- validator.py
+++ validator.py
@@ -1,8 +1,22 @@
-def validate_iban():
-    pass
+def validate_iban(iban):
+    if iban[:2 ] != "TB":
+        return False
+    if len(iban) != 6:
+        return False
+    if not iban[2:].isdigit():
+        return False
+    return True
+    
 
 def validate_balance ():
     pass
 
 def validate_name():
     pass
+
+def validate_input_is_float(input_data):
+    try:
+        float(input_data)
+        return True
+    except ValueError:
+        return False
