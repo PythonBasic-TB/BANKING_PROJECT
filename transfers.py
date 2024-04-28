27f381bd6ba3ab53ba9dac226d57247ef8e43552
paatakill <paata.kilasonia@gmail.com>
2024-04-29 01:58:25 +0400

    transfers.py sruli funqcionali, shida inputebi, validacia, output. tu problemaa shida inputebi mitxarit da gadavaketeb rogorc sachiro iqneba

--- transfers.py
+++ transfers.py
@@ -1,7 +1,26 @@
 from  test_database import customers as database
+from validator import validate_iban, validate_input_is_float
 
-def transfer_money(iban1, iban2, amount):
+def transfer_money():
+    iban1 = input("Enter sender's IBAN: ")
     
+    if not validate_iban(iban1):
+        print(f"Invalid IBAN {iban1}")
+        return
+    
+    iban2 = input("Enter receiver's IBAN: ")
+    
+    if not validate_iban(iban2):
+        print(f"Invalid IBAN {iban2}")
+        return
+    
+    amount = input("Enter amount to transfer: ")
+    if not validate_input_is_float(amount):
+        print("Amount must be a number")
+        return
+    else:
+        amount = float(amount)
+        
     if iban1 not in database:
         print(f"Account {iban1} does not exist")
         
@@ -19,5 +38,3 @@ def transfer_money(iban1, iban2, amount):
         print(f"\nTransfer of {amount} GEL from {iban1} t0 {iban2} was successful")
         print(f"\nThe balance on account {iban1} is {database[iban1]['balance']} GEL")
         print(f"The balance on account {iban2} is {database[iban2]['balance']} GEL")
-
-
