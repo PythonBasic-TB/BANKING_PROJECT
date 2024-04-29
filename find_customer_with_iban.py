3ceddf52ac84d938c60d865cabcafffc20c2c080
VincentTheProxy <167816738+VincentTheProxy@users.noreply.github.com>
2024-04-30 01:42:54 +0400

    Update find_customer_with_iban.py

--- find_customer_with_iban.py
+++ find_customer_with_iban.py
@@ -1,17 +1,7 @@
-from test_database import customers,users
-
-
-# def find_user_with_iban(iban):
-#     for user in users:
-#         if user.get("iban"):
-#             return user
-#     return None
-
-
+from test_database import customers
 
 def find_customer_with_iban(iban):
     for customer in customers:
         if customers.get(f"{iban}"):
             return customers.get(f"{iban}")
     return None
-
