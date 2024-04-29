865ade9561ee28b7787cc28fa277835a63df6d9a
OTOtheCode <okharatishvili@xnet.ge>
2024-04-29 23:27:41 +0400

    hot fix

--- iban_generator.py
+++ iban_generator.py
@@ -1,8 +1,21 @@
 import random
 
 def generate_iban(database):
-	
-	while True:
-		iban =  "TB" + ''.join(random.choices('0123456789', k=4))
-		if iban not in database:
+	ibans = [iban for user in database for iban in user.keys()]
+
+	for i in range(1, 10000):
+		iban = f"TB{i:04d}"
+		if iban not in ibans:
 			return iban
+	raise ValueError("Iban limit reached(TB9999)")
+
+
+
+#database = [{'TB0001': {'name': 'Customer 1', 'surname': 'Doe', 'balance': 100, 'loan': 0, 'interest': 8.2}}, 
+            #{'TB0002': {'name': 'Customer 2', 'surname': 'Doe', 'balance': 200, 'loan': 0, 'interest': 8.2}}, 
+            #{'TB0003': {'name': 'Customer 3', 'surname': 'Doe', 'balance': 300, 'loan': 0, 'interest': 8.2}}, 
+            #{'TB0004': {'name': 'Customer 4', 'surname': 'Doe', 'balance': 400, 'loan': 0, 'interest': 8.2}}, 
+            #{'TB0005': {'name': 'Customer 5', 'surname': 'Doe', 'balance': 500, 'loan': 0, 'interest': 8.2}}]
+
+#iban = generate_iban(database)
+#print(new_iban)
