74d422d8bb03042e86e61a26717ec944e78f666b
VincentTheProxy <167816738+VincentTheProxy@users.noreply.github.com>
2024-05-01 00:48:05 +0400

    Update accaunt_data.py

--- accaunt_data.py
+++ accaunt_data.py
@@ -1,10 +1,9 @@
 account_database = [
-    {
-    'TB0001': {'name': 'Customer 1', 'surname': 'Doe', 'balance': 100, 'loan': 0, 'interest': 8.2}, 
-    'TB0002': {'name': 'Customer 2', 'surname': 'Doe', 'balance': 200, 'loan': 0, 'interest': 8.2}, 
-    'TB0003': {'name': 'Customer 3', 'surname': 'Doe', 'balance': 300, 'loan': 0, 'interest': 8.2}, 
-    }
-]
+    {'TB0001': {'name': 'Customer 1', 'surname': 'Doe', 'balance': 100, 'loan': 0, 'interest': 8.2}}, 
+    {'TB0002': {'name': 'Customer 2', 'surname': 'Doe', 'balance': 200, 'loan': 0, 'interest': 8.2}}, 
+    {'TB0003': {'name': 'Customer 3', 'surname': 'Doe', 'balance': 300, 'loan': 0, 'interest': 8.2}},
+    ]
+
 top_up_hist = []
 loan_hist = []
 p2p_hist = []
