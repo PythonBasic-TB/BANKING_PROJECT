cfa11558cd6151a300b573e23de292e451fcc2e5
J2xani <J2xani@gmail.com>
2024-05-01 01:31:52 +0400

    fixed minor bug

--- find_customer_with_iban.py
+++ find_customer_with_iban.py
@@ -7,12 +7,17 @@ from accaunt_data import account_database
 #             return customers.get(f"{iban}")
 #     return None
 
-
-
 def find_customer_with_iban(iban):
-    for account in account_database:
-        if account_database.get(f"{iban}"):
-            return account_database.get(f"{iban}")
-    return None
+    for ibans in account_database:
+        if iban in ibans:
+            return ibans[iban]
+
+
+# def find_customer_with_iban(iban):
+#     for account in account_database:
+#         if account_database.get(f"{iban}"):
+#             return account_database.get(f"{iban}")
+#     return None
 
 #print(find_customer_with_iban("TB0002"))
+
