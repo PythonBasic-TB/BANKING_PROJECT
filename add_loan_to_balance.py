7921277ec12184147102e530a34cd84c258e917b
OTOtheCode <okharatishvili@xnet.ge>
2024-05-02 03:19:25 +0400

    changed how history is updated

--- add_loan_to_balance.py
+++ add_loan_to_balance.py
@@ -1,6 +1,12 @@
 def add_loan(loan, accountInfo, loan_hist, iban):
     accountInfo['balance'] += loan
     accountInfo['loan'] -= loan
-    historyElement = {'iban' : iban, 'loan' : loan}
-    loan_hist.append(historyElement)
+    #historyElement = {'iban' : iban, 'loan' : loan}
+    ibans ={}
+    if iban not in ibans:
+        ibans[iban]=[]
+        ibans[iban].append(f"loan -{loan}")
+        loan_hist.append(ibans)
+        
     print (f"The loan has been issued successfully. Your balance was filled with {loan} GEL, your balance is: {accountInfo['balance']}")
+    print (loan_hist)
