7cb570d31e14d4d0ea23b1800bf2194530a5ecfc
OTOtheCode <okharatishvili@xnet.ge>
2024-05-02 03:21:52 +0400

    New modules for displaing history of account

--- transaction_history.py
+++ transaction_history.py
@@ -1,8 +1,34 @@
-def view_full_history(): #გადარიცხვები, ბალანსზე თანხის შეტანა, მხოლოდ მომხმარებლის ანგარიშის
-    pass
-def view_top_up_history ():
-    pass
-def view_transfer_history():
-    pass
-def view_loan_history():
-    pass
+from accaunt_data import top_up_hist
+from accaunt_data import loan_hist
+from accaunt_data import p2p_hist
+
+#account_database = [
+   # {'TB0001': {'name': 'Customer 1', 'surname': 'Doe', 'balance': 100, 'loan': 0, 'interest': 8.2}}, 
+    #{'TB0002': {'name': 'Customer 2', 'surname': 'Doe', 'balance': 200, 'loan': 0, 'interest': 8.2}}, 
+   # {'TB0001': {'name': 'Customer 3', 'surname': 'Doe', 'balance': 300, 'loan': 0, 'interest': 8.2}},
+   # ]
+
+
+def view_top_up_history (top_up_hist):
+    iban= input ("Please enter you IBAN: ")
+    for ibans in top_up_hist:
+            if iban  in ibans:
+                for k, v in ibans.items():
+                    if k== iban:
+                        print (f"{k}:{v}")
+
+def view_transfer_history(p2p_hist):
+    iban= input ("Please enter you IBAN: ")
+    for ibans in p2p_hist:
+            if iban  in ibans:
+                for k, v in ibans.items():
+                    if k== iban:
+                        print (f"{k}:{v}")
+def view_loan_history(loan_hist):
+    iban= input ("Please enter you IBAN: ")
+    for ibans in loan_hist:
+            if iban  in ibans:
+                for k, v in ibans.items():
+                    if k== iban:
+                        print (f"{k}:{v}")
+                        
