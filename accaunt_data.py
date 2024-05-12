9cb3104eb263138d525a60cca001fd72f9954683
OTOtheCode <okharatishvili@xnet.ge>
2024-05-13 01:04:10 +0400

    half done with update

--- accaunt_data.py
+++ accaunt_data.py
@@ -1,8 +1,7 @@
 account_database = [
-    {'TB0001': {'name': 'oto', 'surname': 'kharatishvili', 'balance': 100, 'loan': 0, 'interest': 8.2}}, 
+    {'TB0001' : {'name': 'oto', 'surname': 'kharatishvili', 'balance': 100, 'loan': 0, 'interest': 8.2}}, 
     {'TB0002': {'name': 'paata', 'surname': 'kilasonia', 'balance': 200, 'loan': 0, 'interest': 8.2}}, 
-    {'TB0003': {'name': 'revaz', 'surname': 'kiknavelidze', 'balance': 300, 'loan': 0, 'interest': 8.2}},
-    {'TB0004': {'name': 'ani', 'surname': 'basharuli', 'balance': 300, 'loan': 0, 'interest': 8.2}},
+   
     ]
 
 #top_up_hist = []
--- acount_creation.py
+++ acount_creation.py
@@ -1,8 +1,25 @@
 from iban_generator import generate_iban
 from accaunt_data import account_database
 from validator import validate_input_is_float
+import csv
 
+def generate_UID(iban, name, surname): #დასაკორექტირებელი მაქვს, რომ უნიკალური იყოს. IBAN-ს + იანიციალები არ გამოვა.
+    
+    uid = f"{iban}{name[0]}{surname[0]}" 
+	
+    return uid
+	
+
+def write_to_file (dictionary):
+    filename='users.csv'
+    with open(filename, "a", newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(dictionary)
+        
+        
+        return filename
 def add_acount (account_database):
+    
     user_acount ={}
     name = input("Please enter your name: ")
     surname = input("Please enter your surname: ")
@@ -17,6 +34,12 @@ def add_acount (account_database):
         iban = generate_iban(account_database)
         user_acount[iban] = {"name": name, "surname":surname, "balance": balance, "loan": loan,"interest": 8.2}
         account_database.append(user_acount)
+        
+        user = {}
+        user_id = generate_UID(iban, name, surname)
+        user[user_id] = {"iban" : iban, "name": name, "surname":surname, "balance": balance, "loan": loan,"interest": 8.2}
+        
+        write_to_file([user])
         print ((f"Account created successfully! Account Holder: {name} {surname}, Acount number: {iban}, Balance : {balance}GEL"))
         print (account_database)
     
