049fdb72b56650a6d743c46de89d7f73ce764b18
J2xani <J2xani@gmail.com>
2024-05-12 21:12:38 +0400

    add CSV file

--- calculate_anual_interest.py
+++ calculate_anual_interest.py
@@ -1,6 +1,7 @@
 from validator import validate_iban, validate_input_is_float, validate_iban_exists
 from add_loan_to_balance import add_loan
 from find_customer_with_iban import find_customer_with_iban
+import csv
 
 def cal_anual_int (database, loan_hist):
     iban = input("Enter sender's IBAN: ")
@@ -22,10 +23,18 @@ def cal_anual_int (database, loan_hist):
     accountInfo = find_customer_with_iban(iban)
     
     annualRate = loan * accountInfo['interest'] / 100
-    print(f"Your annual interest rate will be {annualRate} GEL")
+    print(f"Your annual interest rate will be {annualRate} GEL")   
+
     answer = input("Proceed with loan Yes/No: ")
     if answer.upper() == "YES":
         add_loan(loan, accountInfo, loan_hist, iban)
+    
+        with open('loanInformation.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            monthlyRate = (loan + annualRate)/12
+            for i in range(1,13):
+                writer.writerow([i,monthlyRate])
+        
     elif answer.upper() == "NO":
         return
     else:
